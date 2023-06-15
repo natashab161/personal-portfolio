a0ce9ed9b21abdbffc3e734f9b74cf0903a69be5
Natasha Bartley <+>
2023-06-14 21:56:25 -0400

    new css for contact page

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -5,23 +5,34 @@ import { Container } from "react-bootstrap";
 function Contactpage() {
   return (
     <div className="contactbackground">
-      <Container>
-        <h2 className="contacthead">Get In Touch</h2>
-        <p className="contactpara">
-          I’m currently searching for opportunities for a front-end developer
-          role. <br /> If there is any vacancy my inbox is always open. Whether
-          <br /> you have any further questions or just want to say hi, <br />
-          I’ll try my best to get back to you!
-        </p>
-        <button
-          className="contactbtn"
-          onClick={() => {
-            window.open("https://www.instagram.com/rahulvijay81/");
-          }}
-        >
-          Say Hello
-        </button>
-        <span></span>
+      <Container className="container">
+        <div className="textbackground row">
+            <div className="col-md-7">
+              <h3 className="aboutmetext">
+                Let's connect!
+              </h3>
+                <p className="aboutdetails">
+                  I’m currently looking to get my software engineering career
+                  started with a company that fosters long term growth!
+                  I'm passionate about utilizng creative technologies
+                  <br /> to expand upon the potential of human creativity 
+                  <br /> to expand upon the potential of human creativity <br />
+                  
+                </p>
+                <button
+                  className="contactbtn"
+                  onClick={() => {
+                    window.open("https://www.instagram.com/rahulvijay81/");
+                  }}
+                >
+                  Say Hello
+                </button>
+                <span></span>
+
+                
+
+            </div>
+        </div>
       </Container>
     </div>
   );
